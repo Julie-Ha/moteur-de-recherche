c51bdfb8b4c57c43dd7737ddefe669326f27a96d
Julie-Ha <69715947+Julie-Ha@users.noreply.github.com>
2020-11-22 13:33:44 +0100

    cosine similarity calculation

--- back/script.py
+++ back/script.py
@@ -1,5 +1,10 @@
 from flask import Flask, jsonify, request 
 from flask_cors import CORS
+import json
+import csv
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import collections
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -10,7 +15,95 @@ def index():
 
 @app.route('/cosine-similarity', methods=['POST'])
 def cosineSimilarity():
-    return jsonify(request.json['entities'])
+    entities = request.json['entities']
+    entities = searchSubEntity(entities)
+
+    wordnetTypes = getWordnetType(entities)
+
+    wordnetVector = createQueryVector(wordnetTypes)
+
+
+    similarities = getCosSimilarity(wordnetVector)
+    similarities = sorted(similarities.items(), key=lambda x: x[1], reverse=True)
+
+    return jsonify(similarities[:10])
+
+#Supprime la string devant le . : computer_user.computer_scientist => computer_scientist
+def entitiesFormate(entities):
+    for i in range(0, len(entities)):
+        if "." in entities[i]:
+            entities[i] = entities[i].split(".")[1]
+    return entities
+
+#Cherche tous les sous entites des entites (seulement les feuilles de l'arbre)
+def searchSubEntity(entities):
+    subEntities = []
+    def getLeafNodes(entity):
+        with open('ontology.csv') as file:
+            reader = csv.reader(file, delimiter=',')
+
+            found = False
+            for row in reader:
+                if row[1] == entity:
+                    found = True
+                    getLeafNodes(row[0])
+                    
+            if not found:
+                subEntities.append(entity)
+
+    for entity in entities:
+        getLeafNodes(entity)
+
+    subEntities = entitiesFormate(subEntities)
+        
+    return subEntities
+
+#Cherche les types wordnet de chaque entite selectionnee dans typeid_name_wordnet.json
+def getWordnetType(entities):
+    wordnetTypes = []
+    with open('typeid_name_wordnet.json') as typeid_name_wordnet:
+        wordnet = json.load(typeid_name_wordnet)
+        
+        for e in entities:
+            for w in wordnet.values():
+                if(e == w["name"]):
+                    wordnetTypes.append(w["type"])
+                    break
+                    
+    return wordnetTypes
+
+#Construit le vector de requete avec les entites selectionees
+def createQueryVector(wordnet):
+    vector = []
+    with open('dimension_name.txt') as dimension_name:
+        lines = dimension_name.readlines()
+
+        
+        for line in lines:
+            found = False
+            for w in wordnet:
+                if w == line.strip():
+                    found = True
+                    break
+            if found:
+                vector.append(1)
+            else:
+                vector.append(0)
+                
+    return vector
+
+#Calcule la cosine similarity entre le QueryVector et chaque article
+def getCosSimilarity(wordnetVector):
+    results = dict()
+    with open('article_vector.json') as article_vector:
+        articles = json.load(article_vector)
+        for (k, v) in articles.items():
+            cos = cosine_similarity([wordnetVector],[v])
+            if cos > 0:
+                results[k] = cos[0][0]
+
+    return results
 
 if __name__ == "__main__":
     app.run()
+
